998ae138602250e1e93d31f0c991286bbdfc0c02
Eric Hofesmann <ehofesmann@gmail.com>
2017-07-14 15:32:03 -0400

    Add files via upload

---
+++ grb_triangles.py
@@ -0,0 +1,17 @@
+
+def generate_triangle(inten_array):
+        #take a flat line (array of all zeros) and make a triangle in the middle of it
+        for i in range(len(inten_array)):
+                if 200 >= i >= 100:
+                        inten_array[i] = i-100
+                elif 300 >= i > 200:
+                        inten_array[i] = 300 - i
+        return inten_array
+
+
+
+
+
+
+triangle_inten = generate_triangle([0]*400)
+print(triangle_inten)
